86f28c9e4e1eae0522fcacb61ada4cff942289d0
gogotanaka <mail@tanakakazuki.com>
2015-10-21 18:55:49 +0900

    商品一覧ページ

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     member do
       get 'purchase'
     end
+    collection do
+      get 'history'
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
